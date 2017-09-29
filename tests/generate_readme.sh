5728cb0b87c0e6e13843bc320d7e14eb8fd67a97
Ai Lin Chia <alc@privacore.com>
2017-09-29 20:17:32 +0200

    Update generate readme script to show which tests does not have automated tests

--- tests/generate_readme.sh
+++ tests/generate_readme.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-find ./ -mindepth 2 -maxdepth 2 -type f -name README |sort -V |xargs -i bash -c "echo {}; head -1q {}; echo ''" > README
+#find ./ -mindepth 2 -maxdepth 2 -type f -name README |sort -V |xargs -i bash -c "echo {}; head -1q {}; echo ''" > README
+
+subdirs=$(find ./ -mindepth 1 -maxdepth 1 -type d |sort -V)
+
+for subdir in $subdirs; do
+	manual=''
+
+	if [ ! -d ${subdir}/testcase ]; then
+		manual=' (manual test)'
+	fi
+
+	echo "${subdir}${manual}"
+
+	if [ -f ${subdir}/README ]; then
+		head -1q ${subdir}/README
+	fi
+	echo ''
+done
